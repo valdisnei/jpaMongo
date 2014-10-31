de0d5cbac0a7ad4aac9ee3cd9189303013788679
valdisnei <valdisnei.fajardo@gmail.com>
2014-10-31 17:55:54 -0200

    projeto mongo com jpa

---
+++ src/main/java/com/valdisSystem/JpaMongo/App.java
@@ -0,0 +1,13 @@
+package com.valdisSystem.JpaMongo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
